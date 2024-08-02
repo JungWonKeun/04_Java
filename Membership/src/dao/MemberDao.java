af54ad652c7b7932f74e9bce3903551c89a4e20c
yukpoyum <anokata@naver.com>
2024-08-02 17:04:59 +0900

    20240802_Java_Membership마저 만들기

--- Membership/src/dao/MemberDao.java
+++ Membership/src/dao/MemberDao.java
@@ -6,12 +6,12 @@ import java.util.List;
 import dto.Member;
 
 // DAO(Data Access Object) : 
-// - 데이터가 저장된 곳(파일/DB)에 접근하는 역할의 객체 
+// - 데이터가 저장된 곳(파일/DB)에 접근하는 역할의 객체
 // - 데이터 저장/수정/삭제/조회 가능
 
 public interface MemberDao {
 
-	// 인터페이스 메서드는
+	// 인터페이스 메서드는 
 	// 묵시적(암묵적)으로 public abstract다!!
 	
 	/**
@@ -19,17 +19,23 @@ public interface MemberDao {
 	 * @return memberList
 	 */
 	List<Member> getMemberList();
+
 	
-		
 	/**
 	 * 회원 추가 
 	 * @param member
 	 * @return true
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	boolean addMember(Member member) throws IOException;
 	
-	/**
+	
+	/** 
+	 * 파일 저장
 	 */
 	void saveFile() throws IOException;
+	
+	
 }
+
+
